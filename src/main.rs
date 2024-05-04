6f9ba8ba50d551361254e20743f3bfb9784ff1c4
klx01 <69247896+klx01@users.noreply.github.com>
2024-05-05 00:12:56 +0400

    stage 6 - get and set commands

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::net::SocketAddr;
+use std::collections::HashMap;
+use std::sync::{Arc, RwLock};
 use std::time::Duration;
 use tokio::io::BufReader;
 use tokio::net::{TcpListener, TcpStream};
@@ -7,21 +8,28 @@ use crate::resp::*;
 
 mod resp;
 
+type Storage = HashMap<Vec<u8>, Vec<u8>>;
+type LockStorage = RwLock<Storage>;
+type ArcStorage = Arc<LockStorage>;
+
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").await
         .expect("Failed to bind to the port");
 
+    let storage: ArcStorage = Default::default();
+
     loop {
-        let (stream, addr) = listener.accept().await
+        let (stream, _addr) = listener.accept().await
             .expect("Failed to accept connection");
+        let storage = Arc::clone(&storage);
         tokio::spawn(async move {
-            handle_connection(stream, addr).await
+            handle_connection(stream, storage).await
         });
     }
 }
 
-async fn handle_connection(mut stream: TcpStream, _addr: SocketAddr) -> Option<()> {
+async fn handle_connection(mut stream: TcpStream, storage: ArcStorage) -> Option<()> {
     loop {
         let command = timeout(
             Duration::from_millis(1000),
@@ -50,7 +58,7 @@ async fn handle_connection(mut stream: TcpStream, _addr: SocketAddr) -> Option<(
 
         let can_continue = timeout(
             Duration::from_millis(1000),
-            handle_command(&mut stream, &command_name, command_params)
+            handle_command(&mut stream, &command_name, command_params, &storage)
         ).await;
         match can_continue {
             Ok(true) => {},
@@ -63,10 +71,12 @@ async fn handle_connection(mut stream: TcpStream, _addr: SocketAddr) -> Option<(
     }
 }
 
-async fn handle_command(mut stream: &mut TcpStream, command_name: &str, command_params: &[Vec<u8>]) -> bool {
+async fn handle_command(mut stream: &mut TcpStream, command_name: &str, command_params: &[Vec<u8>], storage: &LockStorage) -> bool {
     match command_name {
         "PING" => handle_ping(&mut stream).await,
         "ECHO" => handle_echo(&mut stream, command_params).await,
+        "GET" => handle_get(&mut stream, command_params, storage).await,
+        "SET" => handle_set(&mut stream, command_params, storage).await,
         _ => {
             eprintln!("received unknown command {command_name}");
             true
@@ -84,5 +94,36 @@ async fn handle_echo(stream: &mut TcpStream, params: &[Vec<u8>]) -> bool {
         return true;
     }
     write_binary_string(stream, &params[0]).await
+}
+
+async fn handle_get(stream: &mut TcpStream, params: &[Vec<u8>], storage: &LockStorage) -> bool {
+    if params.len() < 1 {
+        eprintln!("get command is missing arguments");
+        return true;
+    }
+    let result = do_get(storage, &params[0]);
+    match result {
+        Some(value) => write_binary_string(stream, &value).await,
+        None => write_null(stream).await,
+    }
+}
+
+fn do_get(storage: &LockStorage, key: &Vec<u8>) -> Option<Vec<u8>> {
+    storage.read().expect("got poisoned lock, can't handle that")
+        .get(key).map(|x| x.clone())
+}
+
+async fn handle_set(stream: &mut TcpStream, params: &[Vec<u8>], storage: &LockStorage) -> bool {
+    if params.len() < 2 {
+        eprintln!("get command is missing arguments");
+        return true;
+    }
+    do_set(storage, params[0].clone(), params[1].clone());
+    write_simple_string(stream, "OK").await
+}
+
+fn do_set(storage: &LockStorage, key: Vec<u8>, value: Vec<u8>) {
+    storage.write().expect("got poisoned lock, can't handle that")
+        .insert(key, value);
 }
 
