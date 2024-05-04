74de550ffb27dce0cffaed5496a123a42b0aba0e
klx01 <69247896+klx01@users.noreply.github.com>
2024-05-04 19:44:07 +0400

    stage 2

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
+use std::io::Write;
 use std::net::TcpListener;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379")
         .expect("Failed to bind to the port");
-    
+
     for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+        let mut stream = match stream {
+            Ok(x) => x,
+            Err(error) => {
+                eprintln!("error in incoming stream: {error}");
+                continue;
             }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+        };
+
+        let result = stream.write_all(b"+PONG\r\n");
+        if let Err(error) = result {
+            eprintln!("failed to write response: {error}");
         }
     }
 }
