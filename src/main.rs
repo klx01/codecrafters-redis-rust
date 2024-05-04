a9bad828791e4d020ee41607b9ca2bd727eda22d
klx01 <69247896+klx01@users.noreply.github.com>
2024-05-04 20:24:06 +0400

    stage 3

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::Write;
+use std::io::{BufRead, BufReader, Write};
 use std::net::TcpListener;
 
 fn main() {
@@ -14,9 +14,25 @@ fn main() {
             }
         };
 
-        let result = stream.write_all(b"+PONG\r\n");
-        if let Err(error) = result {
-            eprintln!("failed to write response: {error}");
+        loop {
+            let mut buf_read = BufReader::new(&stream);
+            let mut read_data = vec![];
+            let line = buf_read.read_until('\n' as u8, &mut read_data);
+            let read_size = match line {
+                Ok(x) => x,
+                Err(error) => {
+                    eprintln!("failed to read data from stream: {error}");
+                    break;
+                }
+            };
+            if read_size == 0 {
+                break;
+            }
+            let result = stream.write_all(b"+PONG\r\n");
+            if let Err(error) = result {
+                eprintln!("failed to write response: {error}");
+                break;
+            }
         }
     }
 }
