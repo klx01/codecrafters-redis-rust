57e458d4672f0ac8c421dc377484d0f80443a5c9
klx01 <69247896+klx01@users.noreply.github.com>
2024-05-04 23:40:53 +0400

    move protocol into a separate file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::net::SocketAddr;
-use tokio::io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader};
+use tokio::io::BufReader;
 use tokio::net::{TcpListener, TcpStream};
+use crate::resp::*;
 
-const DELIMITER: &[u8] = b"\r\n";
+mod resp;
 
 #[tokio::main]
 async fn main() {
@@ -20,7 +21,7 @@ async fn main() {
 
 async fn handle_connection(mut stream: TcpStream, _addr: SocketAddr) -> Option<()> {
     loop {
-        let command = read_command(&mut stream).await?;
+        let command = read_command(&mut BufReader::new(&mut stream)).await?;
         if command.len() == 0 {
             eprintln!("received a command of size 0");
             continue;
@@ -34,132 +35,29 @@ async fn handle_connection(mut stream: TcpStream, _addr: SocketAddr) -> Option<(
             }
         };
         let command_name = command_name.to_ascii_uppercase();
-        match command_name.as_str() {
-            "PING" => handle_ping(&mut stream).await?,
-            "ECHO" => handle_echo(&mut stream, command_params).await?,
+        let can_continue = match command_name.as_str() {
+            "PING" => handle_ping(&mut stream).await,
+            "ECHO" => handle_echo(&mut stream, command_params).await,
             _ => {
                 eprintln!("received unknown command {command_name}");
+                true
             },
-        }
-    }
-}
-
-async fn read_command(stream: &mut TcpStream) -> Option<Vec<Vec<u8>>> {
-    let mut buf_reader = BufReader::new(stream);
-    let array_size = read_command_array_size(&mut buf_reader).await?;
-    let mut command = Vec::with_capacity(array_size);
-    for _ in 0..array_size {
-        command.push(read_binary_string(&mut buf_reader).await?)
-    }
-    Some(command)
-}
-
-async fn read_command_array_size(reader: &mut (impl AsyncBufReadExt + Unpin)) -> Option<usize> {
-    return read_int(reader, "*", true, 100).await
-}
-
-async fn read_int(reader: &mut (impl AsyncBufReadExt + Unpin), expected_type_prefix: &'static str, is_eof_expected: bool, max: usize) -> Option<usize> {
-    let mut buf = String::new();
-    let res = reader.take(10).read_line(&mut buf).await;
-    if let Err(err) = res {
-        eprintln!("failed to read integer line {err}");
-        return None;
-    }
-    if buf.len() == 0 {
-        if !is_eof_expected {
-            eprintln!("unexpected end of file when reading integer");
-        }
-        // EOF
-        return None;
-    }
-    if buf.len() < 4 {
-        eprintln!("invalid format, integer line is too small, len {}, val {buf}", buf.len());
-        return None;
-    }
-    let (data_type, tail) = buf.split_at(1);
-    if data_type != expected_type_prefix {
-        eprintln!("invalid format, expected type {expected_type_prefix}, got {data_type}");
-        return None;
-    }
-    let (size_string, delimiter) = tail.split_at(tail.len() - 2);
-    if delimiter != "\r\n" {
-        eprintln!("invalid format, integer string is too large {buf}");
-        return None;
-    }
-    let int = match size_string.parse() {
-        Ok(x) => x,
-        Err(err) => {
-            eprintln!("failed to parse integer from {size_string}: {err}");
+        };
+        if !can_continue {
             return None;
         }
-    };
-    if int > max {
-        eprintln!("integer {int} larger than max allowed {max}");
-        return None;
     }
-    Some(int)
 }
 
-async fn read_binary_string(reader: &mut (impl AsyncBufReadExt + Unpin)) -> Option<Vec<u8>> {
-    let size = read_binary_string_size(reader).await?;
-    read_binary_string_body(reader, size).await
-}
-
-async fn read_binary_string_size(reader: &mut (impl AsyncBufReadExt + Unpin)) -> Option<usize> {
-    return read_int(reader, "$", true, 300).await
-}
-
-async fn read_binary_string_body(reader: &mut (impl AsyncBufReadExt + Unpin), expected_size: usize) -> Option<Vec<u8>> {
-    let mut result = vec![0; expected_size + DELIMITER.len()];
-    let res = reader.read_exact(&mut result).await;
-    if let Err(err) = res {
-        eprintln!("failed to read string line {err}");
-        return None;
-    }
-    if !result.ends_with(DELIMITER) {
-        eprintln!("invalid format, string is missing the delimiter");
-        return None;
-    }
-    result.truncate(result.len() - DELIMITER.len());
-    Some(result)
-}
-
-async fn handle_ping(stream: &mut TcpStream) -> Option<()> {
+async fn handle_ping(stream: &mut TcpStream) -> bool {
     write_simple_string(stream, "PONG").await
 }
 
-async fn handle_echo(stream: &mut TcpStream, params: &[Vec<u8>]) -> Option<()> {
+async fn handle_echo(stream: &mut TcpStream, params: &[Vec<u8>]) -> bool {
     if params.len() < 1 {
         eprintln!("echo command is missing arguments");
-        return Some(());
+        return true;
     }
     write_binary_string(stream, &params[0]).await
 }
 
-async fn write_simple_string(stream: &mut TcpStream, string: &str) -> Option<()> {
-    let result = stream.write_all(format!("+{string}\r\n").as_bytes()).await;
-    if let Err(error) = result {
-        eprintln!("failed to write simple string: {error}");
-        return None;
-    }
-    Some(())
-}
-
-async fn write_binary_string(stream: &mut TcpStream, string: &[u8]) -> Option<()> {
-    let result = stream.write_all(format!("${}\r\n", string.len()).as_bytes()).await;
-    if let Err(error) = result {
-        eprintln!("failed to write binary string size: {error}");
-        return None;
-    }
-    let result = stream.write_all(string).await;
-    if let Err(error) = result {
-        eprintln!("failed to write binary string body: {error}");
-        return None;
-    }
-    let result = stream.write_all(DELIMITER).await;
-    if let Err(error) = result {
-        eprintln!("failed to write binary string delimiter: {error}");
-        return None;
-    }
-    Some(())
-}
---
+++ src/resp.rs
@@ -0,0 +1,120 @@
+use tokio::io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt};
+
+const DELIMITER_STR: &str = "\r\n";
+const DELIMITER_BYTES: &[u8] = DELIMITER_STR.as_bytes();
+
+pub(crate) async fn read_command(reader: &mut (impl AsyncBufReadExt + Unpin)) -> Option<Vec<Vec<u8>>> {
+    let array_size = read_command_array_size(reader).await?;
+    let mut command = Vec::with_capacity(array_size);
+    for _ in 0..array_size {
+        command.push(read_binary_string(reader).await?)
+    }
+    Some(command)
+}
+
+async fn read_command_array_size(reader: &mut (impl AsyncBufReadExt + Unpin)) -> Option<usize> {
+    return read_int(reader, "*", true, 100).await
+}
+
+async fn read_int(reader: &mut (impl AsyncBufReadExt + Unpin), expected_type_prefix: &'static str, is_eof_expected: bool, max: usize) -> Option<usize> {
+    let mut buf = String::new();
+    let res = reader.take(10).read_line(&mut buf).await;
+    if let Err(err) = res {
+        eprintln!("failed to read integer line {err}");
+        return None;
+    }
+    if buf.len() == 0 {
+        if !is_eof_expected {
+            eprintln!("unexpected end of file when reading integer");
+        }
+        // EOF
+        return None;
+    }
+    if buf.len() < expected_type_prefix.len() + DELIMITER_STR.len() + 1 {
+        eprintln!("invalid format, integer line is too small, len {}, val {buf}", buf.len());
+        return None;
+    }
+    let (data_type, tail) = buf.split_at(expected_type_prefix.len());
+    if data_type != expected_type_prefix {
+        eprintln!("invalid format, expected type {expected_type_prefix}, got {data_type}");
+        return None;
+    }
+    let (size_string, delimiter) = tail.split_at(tail.len() - DELIMITER_STR.len());
+    if delimiter != DELIMITER_STR {
+        eprintln!("invalid format, integer string is too large {buf}");
+        return None;
+    }
+    let int = match size_string.parse() {
+        Ok(x) => x,
+        Err(err) => {
+            eprintln!("failed to parse integer from {size_string}: {err}");
+            return None;
+        }
+    };
+    if int > max {
+        eprintln!("integer {int} larger than max allowed {max}");
+        return None;
+    }
+    Some(int)
+}
+
+async fn read_binary_string(reader: &mut (impl AsyncBufReadExt + Unpin)) -> Option<Vec<u8>> {
+    let size = read_binary_string_size(reader).await?;
+    read_binary_string_body(reader, size).await
+}
+
+async fn read_binary_string_size(reader: &mut (impl AsyncBufReadExt + Unpin)) -> Option<usize> {
+    return read_int(reader, "$", true, 300).await
+}
+
+async fn read_binary_string_body(reader: &mut (impl AsyncBufReadExt + Unpin), expected_size: usize) -> Option<Vec<u8>> {
+    let mut result = vec![0; expected_size + DELIMITER_BYTES.len()];
+    let res = reader.read_exact(&mut result).await;
+    if let Err(err) = res {
+        eprintln!("failed to read string line {err}");
+        return None;
+    }
+    if !result.ends_with(DELIMITER_BYTES) {
+        eprintln!("invalid format, string is missing the delimiter");
+        return None;
+    }
+    result.truncate(result.len() - DELIMITER_BYTES.len());
+    Some(result)
+}
+
+pub(crate) async fn write_simple_string(stream: &mut (impl AsyncWriteExt + Unpin), string: &str) -> bool {
+    let result = stream.write_all(format!("+{string}{DELIMITER_STR}").as_bytes()).await;
+    if let Err(error) = result {
+        eprintln!("failed to write simple string: {error}");
+        return false;
+    }
+    true
+}
+
+pub(crate) async fn write_binary_string(stream: &mut (impl AsyncWriteExt + Unpin), string: &[u8]) -> bool {
+    let result = stream.write_all(format!("${}{DELIMITER_STR}", string.len()).as_bytes()).await;
+    if let Err(error) = result {
+        eprintln!("failed to write binary string size: {error}");
+        return false;
+    }
+    let result = stream.write_all(string).await;
+    if let Err(error) = result {
+        eprintln!("failed to write binary string body: {error}");
+        return false;
+    }
+    let result = stream.write_all(DELIMITER_BYTES).await;
+    if let Err(error) = result {
+        eprintln!("failed to write binary string delimiter: {error}");
+        return false;
+    }
+    true
+}
+
+pub(crate) async fn write_null(stream: &mut (impl AsyncWriteExt + Unpin)) -> bool {
+    let result = stream.write_all(format!("$-1{DELIMITER_STR}").as_bytes()).await;
+    if let Err(error) = result {
+        eprintln!("failed to write simple string: {error}");
+        return false;
+    }
+    true
+}
